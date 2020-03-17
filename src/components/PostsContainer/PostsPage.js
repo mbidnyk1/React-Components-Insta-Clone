20d5b533a23cd397e5f10e67c828e85938b00fc8
Michael Bidnyk <mbidnyk1@gmail.com>
2020-03-17 18:01:39 -0500

    search bar and posts are rendering props are getting data for each component

--- src/components/PostsContainer/PostsPage.js
+++ src/components/PostsContainer/PostsPage.js
@@ -3,19 +3,16 @@
 import React, { useState } from "react";
 import Post from "./Post";
 import "./Posts.css";
+import dummyData from '../../dummy-data';
 // import data 
-import dummyData from "../dummy-data";
   // set up state for your data
-  const [data] = useState(dummyData);
-  console.log(data);
   const PostsPage = () => {
+    const [data] = useState(dummyData);
   return (
     <div className="posts-container-wrapper">
       {/* map through data here to return a Post and pass data as props to Post */
       data.map(p => (
-        <div className="posts-container-wrapper">
        <Post postData={p} />
-       </div>
       ))}
     </div>
   );
